3387c74a5f0906941b91a1f051dadc29b5fe4076
RabbitTone <jose02579@gmail.com>
2022-03-02 15:42:42 -0700

    added comment along with test code to further understand the MSP430. Updated README.md

--- demos/1-led/README.md
+++ demos/1-led/README.md
@@ -6,5 +6,8 @@ Be sure to understand the manipulation of P1OUT and P1DIR.
 ## Suggested Activities
 
 * Modify so that different combinations of LEDs are illuminated.
-
+  Made a loop that switches between LED_GREEN and LED_RED on a delay cycle of
+  0.25 seconds
 * Determine the impact of not setting one of the LED bits in P1DIR (and why). 
+  Not setting the either both or one of the LED bits in P1DIR will not allow
+  for their use regardless of P1OUT
--- demos/1-led/main.c
+++ demos/1-led/main.c
@@ -3,10 +3,27 @@
 #include "libTimer.h"
 #include "led.h"
 
-int main(void) {
-  P1DIR |= LEDS;
-  P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
+//void one_min_delay();
 
+int main(void) {
+  P1DIR |= LEDS;   //set both LEDS
+  P1OUT |= (LED_GREEN | LED_RED);   //turn on GREEN & RED LED
+  P1OUT &= ~(LED_GREEN | LED_RED);  //turn off GREEN & RED LED
+  
   or_sr(0x18);		/* CPU off, GIE on */
+}
+/*
+Void one_min_delay(){
+  __delay_cycles(2500000); //2.5 seconds
+  __delay_cycles(2500000);
+  __delay_cycles(2500000);
+  __delay_cycles(2500000);
+}
+*/
+
+void fliker(){
+  while(1){
+    P1OUT ^= (LED_GREEN | LED_RED);
+    __delay_cycles(250000);
+  }
 }
