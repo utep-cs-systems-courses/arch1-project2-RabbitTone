004871c7d1229d1ed7667e89e3797dcb2fca61f4
RabbitTone <jose02579@gmail.com>
2022-03-08 18:02:43 -0700

    added a switch case to act as a state machine

--- demos/4-led-blink-pattern/main.c
+++ demos/4-led-blink-pattern/main.c
@@ -3,10 +3,13 @@
 #include "libTimer.h"
 #include "led.h"
 
+#define NORMAL 250 //clock speed
+#define FAST NORMAL / 2
+#define SLOW NORMAL * 2
 int main(void) {
   P1DIR |= LEDS;
   P1OUT &= ~LED_GREEN;
-  P1OUT |= LED_RED;
+  P1OUT &= ~LED_RED;
 
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   enableWDTInterrupts();	/* enable periodic interrupt */
@@ -15,28 +18,64 @@ int main(void) {
 }
 
 // global state vars that control blinking
-int blinkLimit = 5;  // duty cycle = 1/blinkLimit
+int blinkLimit = 0;  // duty cycle = 1/blinkLimit
 int blinkCount = 0;  // cycles 0...blinkLimit-1
 int secondCount = 0; // state var representing repeating time 0…1s
+int colors = 0;
 
-void
-__interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
-{
-  // handle blinking 
+void blink_RLED(){
+  blinkCount ++;
+  if (blinkCount >= blinkLimit) { // on for 1 interrupt period
+    blinkCount = 0;
+    P1OUT |= LED_RED;
+  } else{	          // off for blinkLimit - 1 interrupt periods
+    P1OUT &= ~LED_RED;
+  }
+}
+
+void blink_GLED(){
   blinkCount ++;
   if (blinkCount >= blinkLimit) { // on for 1 interrupt period
     blinkCount = 0;
     P1OUT |= LED_GREEN;
-  } else		          // off for blinkLimit - 1 interrupt periods
+  } else{	          // off for blinkLimit - 1 interrupt periods
     P1OUT &= ~LED_GREEN;
+  }
+}
 
-  // measure a second
+void clock(){
   secondCount ++;
-  if (secondCount >= 250) {  // once each second
+  if (secondCount >= FAST) {  // once each second
     secondCount = 0;
     blinkLimit ++;	     // reduce duty cycle
-    if (blinkLimit >= 8)     // but don't let duty cycle go below 1/7.
+    if (blinkLimit >= 15){     // but don't let duty cycle go below 1/7.
       blinkLimit = 0;
+      if(colors >= 1){
+	colors = 0;
+      }else{
+	colors ++;
+      }
+    }
   }
+}
+
+void statemachine(){
+  switch(colors){
+  case 0:
+    blink_GLED();
+    break;
+  default:
+    blink_RLED();
+    break;
+  }
+}
+ 
+void
+__interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
+{
+  // handle blinking
+  statemachine();
+  // measure a second
+  clock();
 } 
 
